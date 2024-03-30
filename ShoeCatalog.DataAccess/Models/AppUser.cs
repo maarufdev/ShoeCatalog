29793be72d80dab7dee8ad6219ca422a51f1ecd0
maarufdev <maarufdev1231@gmail.com>
2024-03-30 19:21:45 +0800

    Added initial Model for authentications

---
+++ ShoeCatalog.DataAccess/Models/AppUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeCatalog.Domain.Models
+{
+    public class AppUser
+    {
+        public AppUser()
+        {
+            
+        }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? MiddleName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        public string? ImageProfile { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ ShoeCatalog.DataAccess/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeCatalog.Domain.Models
+{
+    public class Cart
+    {
+        [Key]
+        public string? Id { get; set; }
+
+        public string? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string? OrderedBy { get; set; }
+        public DateTime OrderedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
